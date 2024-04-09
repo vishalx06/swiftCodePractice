553b3c6b333d227c11670fb9fc67bc3fa544927c
vishalx06 <vishalx06@gmail.com>
2024-04-09 18:19:48 +0530

    Initial Commit

---
+++ swiftpractice/swiftpracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftpracticeApp.swift
+//  swiftpractice
+//
+//  Created by Vishal Sharma on 09/04/24.
+//
+
+import SwiftUI
+
+@main
+struct swiftpracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
